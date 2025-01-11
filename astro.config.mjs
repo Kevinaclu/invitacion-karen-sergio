f536c8b5eace1ee46a58a5c277a746b39e3f6db1
kevinaclu <kevinaclu@gmail.com>
2025-01-10 20:00:37 -0700

    add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   devToolbar: { enabled: false },
+  adapter: vercel(),
+  output: "server",
 });
